15d299ecdb726f030a8ef164e7068ea43af32d03
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2022-10-29 19:05:28 +0200

    termporary disable auth for testing purpose

--- routers/event.js
+++ routers/event.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getEvents, addEvent } from "../controllers/event.js";
-import auth from "../middleware/auth.js";
+// import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/", getEvents);
-router.post("/", auth, addEvent);
+router.post("/", addEvent);
+// router.post("/", auth, addEvent);
 
 export default router;
